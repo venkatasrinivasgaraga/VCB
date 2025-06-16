f96c086a4e4eed58bfb8536ce271c8d60a70edd9
Garaga venkata srinivas <104828717+venkatasrinivasgaraga@users.noreply.github.com>
2025-06-16 16:44:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 16978078))
-API_HASH = os.getenv("API_HASH", "91ccaf748f031b656bbf64ff47f990e3")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7669627881:AAEXDQ-bHos23Bdadf1qasNvK0cnDayT7NI")
+API_ID = int(os.getenv("API_ID", ))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_URI = os.getenv("MONGO_URI", "")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1002518162634))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ))
